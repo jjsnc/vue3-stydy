c39cdf9faa674a8d50f745873620319416202c6a
早起喝燕麦 <zhuowangyi@163.com>
2023-07-07 11:42:41 +0800

    init

--- src/components/animationA.vue
+++ src/components/animationA.vue
@@ -1,3 +1,48 @@
+<script setup>
+import { ref } from "vue";
+
+const disabled = ref(false);
+
+function warnDisabled() {
+  disabled.value = true;
+
+  setTimeout(() => {
+    disabled.value = false;
+  }, 1500);
+}
+</script>
 <template>
-    <div>he</div>
+  <div class="ad-1">
+    <div :class="{ shake: disabled }">
+      <button @click="warnDisabled">click Me</button>
+      <p>This feature is disabled!</p>
+    </div>
+  </div>
 </template>
+
+<style>
+.shake {
+  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  transform: translate3d(0, 0, 0);
+}
+
+@keyframes shake {
+  10%,
+  90% {
+    transform: translate3d(-1px, 0 0);
+  }
+  20%,
+  80% {
+    transform: translate3d(2px, 0, 0);
+  }
+  30%,
+  50%,
+  70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+  40%,
+  60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
+</style>
