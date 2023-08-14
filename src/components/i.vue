4b10da1e5397ce3e911c2989757ecef190bcec4f
早起喝燕麦 <zhuowangyi@163.com>
2023-08-14 11:25:33 +0800

    init

--- src/components/i.vue
+++ src/components/i.vue
@@ -1,24 +1,25 @@
 <template>
   <div>
-    
     <h1>{{ $translate("greetings.hello") }}</h1>
-    <j v-model:title.capitalize="testValue" v-model:lastName="name" ></j>  {{testValue}} {{name}} 
-
-
-    
-   </div>
+    <j v-model:title.capitalize="testValue" v-model:lastName="name" @submit="handleSubmit"></j>
+    {{ testValue }} {{ name }}
+  </div>
 </template>
 
 <script setup>
-
-import J from './j.vue'
-import { inject,ref } from "vue";
+import J from "./j.vue";
+import { inject, ref } from "vue";
 
 const i18n = inject("i18n");
 
-let  testValue = ref('test');
-let  name = ref('asf');
+let testValue = ref("test");
+let name = ref("asf");
 
-let age =ref('18')
+let age = ref("18");
 console.log(i18n.greetings.hello);
+
+
+function handleSubmit(email,password){
+  console.log(email,password)
+}
 </script>
--- src/components/j.vue
+++ src/components/j.vue
@@ -1,35 +1,47 @@
 <script setup>
+import { computed } from "vue";
 
-import {computed} from 'vue';
-
-
-const props = defineProps(['title','lastName','titleModifiers'])
-const emit = defineEmits(['update:title','update:lastName'])
+const props = defineProps(["title", "lastName", "titleModifiers"]);
+const emit = defineEmits({
+  "update:title": null,
+  "update:lastName": null,
+  submit: ({ email, password }) => {
+    if (email && password) {
+      return true;
+    } else {
+      console.warn("Invalid submit event payload!");
+      return false;
+    }
+  },
+  click: null,
+});
 
 function emitValue(e) {
-  let value = e.target.value
+  let value = e.target.value;
   if (props.titleModifiers.capitalize) {
-    value = value.charAt(0).toUpperCase() + value.slice(1)
+    value = value.charAt(0).toUpperCase() + value.slice(1);
   }
-  emit('update:title', value)
+  emit("update:title", value);
 }
-
-
 
 const myName = computed({
   get() {
-    return props.lastName
+    return props.lastName;
   },
   set(value) {
-    emit('update:lastName', value)
-  }
-})
+    emit("update:lastName", value);
+  },
+});
 
-
+function submitForm(email, password) {
+  emit("submit", { email, password });
+}
 </script>
 
 <template>
   <input type="text" :value="title" @input="emitValue" />
 
-
+  <el-button @click="submitForm('zhuowangyi@163.com', '2013')"
+    >测试效验按钮</el-button
+  >
 </template>
