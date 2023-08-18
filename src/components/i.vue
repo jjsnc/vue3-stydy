0470fb89d73ee7089181af67b9272832fc6ac80c
早起喝燕麦 <zhuowangyi@163.com>
2023-08-18 10:35:45 +0800

    init

--- src/components/i.vue
+++ src/components/i.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-    <h1>{{ $translate("greetings.hello") }}</h1>
-    <j v-model:title.capitalize="testValue" v-model:lastName="name" @submit="handleSubmit"></j>
-    {{ testValue }} {{ name }}
+
+    <ul>
+      <li v-for="item in list" ref="itemRefs">
+        {{ item }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup>
 import J from "./j.vue";
-import { inject, ref } from "vue";
+import { ref, onMounted, inject } from "vue";
+
+const list = ref([1, 2, 3]);
+
+const itemRefs = ref([]);
 
 const i18n = inject("i18n");
 
@@ -18,8 +25,12 @@ let name = ref("asf");
 let age = ref("18");
 console.log(i18n.greetings.hello);
 
-
-function handleSubmit(email,password){
-  console.log(email,password)
+function handleSubmit(email, password) {
+  console.log(email, password);
 }
+
+onMounted(() => {
+  console.log(itemRefs.value[0])
+  alert(itemRefs.value.map((i) => i.textContent));
+});
 </script>
