fd7249c4ca0a586cbe06083325c26b864c71df1f
早起喝燕麦 <zhuowangyi@163.com>
2024-08-22 10:55:44 +0800

    init

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,32 +1,34 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
+
 import Home from "../components/HelloWorld.vue";
-import A from "../components/animationA.vue";
-import B from "../components/animationB.vue";
-import C from "../components/C.vue";
-import D from "../components/d.vue";
-import E from "../components/e.vue";
-import F from "../components/f.vue";
-import G from "../components/g.vue";
-import H from "../components/h.vue";
-import I from "../components/i.vue";
-import WatchPage from "../views/watch/index.vue";
-import WatchEffectPage from "../views/watchEffect/index.vue";
-import directivesPage from "../views/directives/index.vue";
+import animationA from "@/components/animationA.vue";
+// import A from "../components/animationA.vue";
+// import B from "../components/animationB.vue";
+// import C from "../components/C.vue";
+// import D from "../components/d.vue";
+// import E from "../components/e.vue";
+// import F from "../components/f.vue";
+// import G from "../components/g.vue";
+// import H from "../components/h.vue";
+// import I from "../components/i.vue";
+// import WatchPage from "../views/watch/index.vue";
+// import WatchEffectPage from "../views/watchEffect/index.vue";
+// import directivesPage from "../views/directives/index.vue";
 
 const routes = [
   { path: "/", component: Home },
-  { path: "/a", component: A },
-  { path: "/b", component: B },
-  { path: "/d", component: D },
-  { path: "/e", component: E },
-  { path: "/f", component: F },
-  { path: "/g", component: G },
-  { path: "/h", component: H },
-  { path: "/i", component: I },
-  { path: "/watch", component: WatchPage },
-  { path: "/watchEffect", component: WatchEffectPage },
-  { path: "/directives", component: directivesPage },
+  { path: "/a", component: animationA },
+  // { path: "/b", component: B },
+  // { path: "/d", component: D },
+  // { path: "/e", component: E },
+  // { path: "/f", component: F },
+  // { path: "/g", component: G },
+  // { path: "/h", component: H },
+  // { path: "/i", component: I },
+  // { path: "/watch", component: WatchPage },
+  // { path: "/watchEffect", component: WatchEffectPage },
+  // { path: "/directives", component: directivesPage },
 ];
 
 const router = createRouter({
